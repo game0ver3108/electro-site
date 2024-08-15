796d93567e923c49091b794450bc20abc4a45bb9
game0ver3108 <121425922+game0ver3108@users.noreply.github.com>
2024-08-15 15:51:33 +1000

    remove

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,3 @@
 document.getElementById("calc").addEventListener("submit", (e) => {
     e.preventDefault();
-    const volts = Number(e.target["vVal"].value);
-    const amps = Number(e.target["iVal"].value);
-    alert(volts+amps);
 });
-
-function funny() {
-    alert("Hhhhhhhhhhhheeeeeeeeeeyyyyyyyyyyyyyyyyyyyyyy")
-}
